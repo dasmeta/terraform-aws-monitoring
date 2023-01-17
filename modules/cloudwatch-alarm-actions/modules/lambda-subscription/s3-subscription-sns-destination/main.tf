9091e311f349839edf283c5bc22a8826565aebd4
Aram Karapetyan <aram@dasmeta.com>
2023-01-17 15:28:02 +0400

    fix(DMVP-cmdb): Add Lambda for CMDB

--- modules/cloudwatch-alarm-actions/modules/lambda-subscription/s3-subscription-sns-destination/main.tf
+++ modules/cloudwatch-alarm-actions/modules/lambda-subscription/s3-subscription-sns-destination/main.tf
@@ -8,8 +8,6 @@ resource "aws_s3_bucket_notification" "incoming" {
   lambda_function {
     lambda_function_arn = var.lambda_function_arn
     events              = ["s3:ObjectCreated:Put"]
-    # filter_prefix       = "foldername"
-    # filter_suffix       = ".zip"
   }
 
   depends_on = [aws_lambda_permission.s3_permission_to_trigger_lambda]
