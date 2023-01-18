3c77672ca6210698a64a1c00ed7cee71fa62496e
Krashchenko <vitalii.krashchenko@mbp0434.fritz.box>
2023-01-18 14:23:48 +0100

    added archiving lambda source code

--- modules/cloudwatch-alarm-actions/modules/lambda-subscription/main.tf
+++ modules/cloudwatch-alarm-actions/modules/lambda-subscription/main.tf
@@ -1,22 +1,28 @@
+locals {
+  lambda_function_file_path   = "${path.module}/src/${var.type}/lambda.py"
+  lambda_function_hash        = filesha256(local.lambda_function_file_path)
+  lambda_function_output_path = "builds/lambda-${local.lambda_function_hash}.zip"
+}
+
 data "archive_file" "lambda_code_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/src/${var.type}"
-  output_path = "${path.module}/src/${var.type}.zip"
+  source_file = local.lambda_function_file_path
+  output_path = local.lambda_function_output_path
 }
 
 module "lambda" {
   source  = "terraform-aws-modules/lambda/aws"
   version = "4.7.1"
 
-  create        = true
-  function_name = substr(replace("${var.sns_topic_name}-${var.uniq_id}-${var.type}", ".", "-"), 0, 63)
-  handler       = "lambda.handler"
-  runtime       = "python3.7"
-  memory_size   = var.memory_size
-  timeout       = var.timeout
-  publish       = true
-#  source_path   = "${path.module}/src/${var.type}"
-  local_existing_package = data.archive_file.lambda_code_zip.output_path
+  function_name           = substr(replace("${var.sns_topic_name}-${var.uniq_id}-${var.type}", ".", "-"), 0, 63)
+  handler                 = "lambda.handler"
+  runtime                 = "python3.7"
+  memory_size             = var.memory_size
+  timeout                 = var.timeout
+#  publish                 = true
+  create_package          = false
+  ignore_source_code_hash = true
+  local_existing_package  = local.lambda_function_output_path
 
   # Create and use an IAM role which can log function output to CloudWatch,
   # plus the custom policy which can copy ALB logs from S3 to CloudWatch.
