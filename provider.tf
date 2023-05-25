7d89c75e5cbc4a0647274faeccbd8b77f452bd2e
Julia Aghamyan <julia@dasmeta.com>
2023-05-25 17:10:16 +0400

    fix(DMVP-lambda-alert): Add monitoring module

--- provider.tf
+++ provider.tf
@@ -6,13 +6,3 @@ provider "aws" {
   region = "us-east-1"
   alias  = "virginia"
 }
-
-
-terraform {
-  required_version = ">= 1.3.0"
-  required_providers {
-    aws = {
-      version = "~>4.50"
-    }
-  }
-}
---
+++ version.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws = {
+      version = "~>4.50"
+    }
+  }
+}
