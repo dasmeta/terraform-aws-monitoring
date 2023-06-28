b9805404eca13f4ec16568779138614a3d7b8c3b
Julia Aghamyan <julia@dasmeta.com>
2023-06-28 10:38:51 +0400

    feat(DMVP-expression-alerts): Change lambda for handle expression

--- modules/cloudwatch-alarm-actions/modules/lambda-subscription/src/teams/lambda.py
+++ modules/cloudwatch-alarm-actions/modules/lambda-subscription/src/teams/lambda.py
@@ -33,10 +33,228 @@ def guess_subject(event):
             "Message subject not included in SNS, using fallback message subject")
         return FALLBACK_SUBJECT
 
+def event_handler_for_metrics(metrics_body):
+
+    for metrics_this in metrics_body:
+        if  'MetricStat' in metrics_this:
+            metric_stat = metrics_this["MetricStat"]
+
+    metric = metric_stat["Metric"]
+    metric_name = metric["MetricName"]
+    metric_namespace = metric["Namespace"]
+
+    dimension_string = ""
+    MetricWidget = {}
+
+    for dimension_object in metric["Dimensions"]:
+        dimension_string += dimension_object["name"] + \
+            "/" + dimension_object["value"] + "/"
+
+    if len(metric["Dimensions"]) == 1:
+        MetricWidget = {
+            "width": 600,
+            "height": 395,
+            "metrics": [
+                [
+                    metric["Namespace"],
+                    metric_name,
+                    metric["Dimensions"][0]["name"],
+                    metric["Dimensions"][0]["value"],
+                    {
+                        "stat": "Average"
+                    }
+                ]
+            ],
+            "period": 300,
+            "view": "timeSeries"
+        }
+    elif not len(metric["Dimensions"]):
+        MetricWidget = {
+            "width": 300,
+            "height": 200,
+            "metrics": [
+                [
+                    metric["Namespace"],
+                    metric_name,
+                    metric["MetricName"],
+                    metric["Namespace"],
+                    {
+                        "stat": "Average"
+                    }
+                ]
+            ],
+            "period": 300,
+            "view": "timeSeries"
+        }
+    else:
+        MetricWidget = {
+            "width": 600,
+            "height": 395,
+            "metrics": [
+                [
+                    metric["Namespace"],
+                    metric_name,
+                    metric["Dimensions"][1]["name"],
+                    metric["Dimensions"][1]["value"],
+                    metric["Dimensions"][2]["name"],
+                    metric["Dimensions"][2]["value"],
+                    metric["Dimensions"][0]["name"],
+                    metric["Dimensions"][0]["value"],
+                    {
+                        "stat": "Average"
+                    }
+                ]
+            ],
+            "period": 300,
+            "view": "timeSeries"
+        }
+    return MetricWidget,dimension_string,metric_namespace,metric_name
+
+def event_handler_for_expression(metrics_body):
+    MetricWidget = {}
+    metrics_widget = []
+    metric_namespace = []
+    metric_name = []
+
+    for metrics_this in metrics_body:
+        temp_metric = {}
+        if  'Expression' in metrics_this:
+            expression = metrics_this["Expression"]
+        if  'MetricStat' in metrics_this:
+            metric_stat = metrics_this["MetricStat"]
+            metric = metric_stat["Metric"]
+            temp_metric["Namespace"] = metric["Namespace"]
+            temp_metric["MetricName"] = metric["MetricName"]
+            temp_metric["id"] = metrics_this["Id"]
+            temp_metric["Name"]  = str(metric["Dimensions"][0]["name"])
+            temp_metric["Value"] = str(metric["Dimensions"][0]["value"])
+
+            metric_name.append(temp_metric["MetricName"])
+            metric_namespace.append(temp_metric["Namespace"])
+            metrics_widget.append(temp_metric)
+    metrics = []
+    for item in metrics_widget:
+        temp = []
+        temp.append( item["Namespace"])
+        temp.append( item["MetricName"])
+        temp.append( item["Name"])
+        temp.append( item["Value"])
+        temp.append( {'id': item["id"]})
+
+        print(temp)
+        metrics.append(temp)
+
+    metrics.append([{"expression" : expression}])
+
+
+    MetricWidget = {
+        "width": 600,
+        "height": 395,
+        "metrics": metrics,
+        "period": 300,
+        "view": "timeSeries"
+    }
+    return MetricWidget,expression,metric_namespace,metric_name
+
+def payload(alert_type,subject,aws_account,dimension_string,metric_namespace,metric_name,image,url):
+
+    # Check subject and add smile)
+    ok_check = "OK" in subject
+    if ok_check:
+        smile_subject = "✅ " + subject
+    else:
+        smile_subject = "❌ " + subject
+
+    if alert_type == "Expression":
+        items = [
+                    {
+                        "type": "FactSet",
+                        "facts": [{
+                            "title": "AccountId",
+                            "value": aws_account
+                        }, {
+                            "title": "Expression",
+                            "value": dimension_string
+                        },{
+                            "title": "Metics",
+                            "value": str(metric_name)
+                        },{
+                            "title": "Namespaces",
+                            "value": str(metric_namespace)
+                        }]
+                    }
+                ]
+        print("Items: ", items)
+    else:
+        items = [
+                    {
+                        "type": "FactSet",
+                        "facts": [{
+                            "title": "AccountId",
+                            "value": aws_account
+                        }, {
+                            "title": "Dimensions",
+                            "value": dimension_string
+                        }, {
+                            "title": "Metic",
+                            "value": metric_namespace + "/" + metric_name
+                        }]
+                    }
+                ]
+
+    payload = {
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "contentUrl": "contentUrl",
+                "content": {
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "type": "AdaptiveCard",
+                    "title": subject,
+                    "version": "1.2",
+                    "msteams": {
+                        "width": "Full"
+                    },
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "text": smile_subject,
+                            "size": "Large",
+                            "weight": "Bolder",
+                            "style": "heading"
+                        },
+                        {
+                            "type": "Container",
+                            "items": items
+                        },
+                        {
+                            "type": "Image",
+                            "url": image
+                        },
+                        {
+                            "type": "ActionSet",
+                            "actions": [
+                                {
+                                    "type": "Action.OpenUrl",
+                                    "title": "See more",
+                                    "url": url
+                                }
+                            ]
+                        }
+                    ]
+
+                }
+            }
+        ]
+    }
+    return payload
 
 def handler(event, context):
     """send message via teams"""
 
+    print("Event",event)
+    print("Context",context)
     teams_webhook_url = os.environ['WEBHOOK_URL']
     url = "https://" + os.environ['REGION'] + ".console.aws.amazon.com/cloudwatch/home?region=" + \
         os.environ['REGION'] + "#alarmsV2:?~(alarmStateFilter~%27ALARM)"
@@ -59,82 +277,23 @@ def handler(event, context):
     if "Metrics" in trigger_body:
         metrics_body = trigger_body["Metrics"]
         logger.debug("> Alarm Metrics Body Value", metrics_body)
-
+        print("metrics_body: ",metrics_body)
         # processing one single metric
         # TODO: we need to manage multiple metrics processing
-        for metrics_this in metrics_body:
-            try:
-                metric_stat = metrics_this["MetricStat"]
+        # for metrics_this in metrics_body:
+        #     try:
+        #         metric_stat = metrics_this["MetricStat"]
 
-            except KeyError:
-                logger.debug("MetricStat is missing in this metric block")
-
-        metric = metric_stat["Metric"]
-        metric_name = metric["MetricName"]
-        metric_namespace = metric["Namespace"]
-
-        for dimension_object in metric["Dimensions"]:
-            dimension_string += dimension_object["name"] + \
-                "/" + dimension_object["value"] + "/"
-
-        if len(metric["Dimensions"]) == 1:
-            MetricWidget = {
-                "width": 600,
-                "height": 395,
-                "metrics": [
-                    [
-                        metric["Namespace"],
-                        metric_name,
-                        metric["Dimensions"][0]["name"],
-                        metric["Dimensions"][0]["value"],
-                        {
-                            "stat": "Average"
-                        }
-                    ]
-                ],
-                "period": 300,
-                "view": "timeSeries"
-            }
-        elif not len(metric["Dimensions"]):
-            MetricWidget = {
-                "width": 300,
-                "height": 200,
-                "metrics": [
-                    [
-                        metric["Namespace"],
-                        metric_name,
-                        metric["MetricName"],
-                        metric["Namespace"],
-                        {
-                            "stat": "Average"
-                        }
-                    ]
-                ],
-                "period": 300,
-                "view": "timeSeries"
-            }
-        else:
-            MetricWidget = {
-                "width": 600,
-                "height": 395,
-                "metrics": [
-                    [
-                        metric["Namespace"],
-                        metric_name,
-                        metric["Dimensions"][1]["name"],
-                        metric["Dimensions"][1]["value"],
-                        metric["Dimensions"][2]["name"],
-                        metric["Dimensions"][2]["value"],
-                        metric["Dimensions"][0]["name"],
-                        metric["Dimensions"][0]["value"],
-                        {
-                            "stat": "Average"
-                        }
-                    ]
-                ],
-                "period": 300,
-                "view": "timeSeries"
-            }
+        #     except KeyError:
+        #         logger.debug("MetricStat is missing in this metric block")
+        for metric_this in metrics_body:
+            if metric_this["ReturnData"] == True:
+                if "Expression" in metric_this:
+                    MetricWidget,dimension_string,metric_namespace,metric_name = event_handler_for_expression(metrics_body)
+                    alert_type = "Expression"
+                else:
+                    alert_type = "Metric"
+                    MetricWidget,dimension_string,metric_namespace,metric_name = event_handler_for_metrics(metrics_body)
 
     else:
         for dimension_object in trigger_body["Dimensions"]:
@@ -169,83 +328,16 @@ def handler(event, context):
         response["MetricWidgetImage"]).decode('utf-8')
     image = 'data:image/png;base64, {}'.format(encoded_data)
 
-    # Check subject and add smile)
-    ok_check = "OK" in subject
-    if ok_check:
-        smile_subject = "✅ " + subject
-    else:
-        smile_subject = "❌ " + subject
-
-    # Teams create AdaptiveCard and send
-    payload = {
-        "type": "message",
-        "attachments": [
-            {
-                "contentType": "application/vnd.microsoft.card.adaptive",
-                "contentUrl": "contentUrl",
-                "content": {
-                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                    "type": "AdaptiveCard",
-                    "title": subject,
-                    "version": "1.2",
-                    "msteams": {
-                        "width": "Full"
-                    },
-                    "body": [
-                        {
-                            "type": "TextBlock",
-                            "text": smile_subject,
-                            "size": "Large",
-                            "weight": "Bolder",
-                            "style": "heading"
-                        },
-                        {
-                            "type": "Container",
-                            "items": [
-                                {
-                                    "type": "FactSet",
-                                    "facts": [{
-                                        "title": "AccountId",
-                                        "value": aws_account
-                                    }, {
-                                        "title": "Dimensions",
-                                        "value": dimension_string
-                                    }, {
-                                        "title": "Metic",
-                                        "value": metric_namespace + "/" + metric_name
-                                    }]
-                                }
-                            ]
-                        },
-                        {
-                            "type": "Image",
-                            "url": image
-                        },
-                        {
-                            "type": "ActionSet",
-                            "actions": [
-                                {
-                                    "type": "Action.OpenUrl",
-                                    "title": "See more",
-                                    "url": url
-                                }
-                            ]
-                        }
-                    ]
-
-                }
-            }
-        ]
-    }
+    payload_data = payload(alert_type,subject,aws_account,dimension_string,metric_namespace,metric_name,image,url)
 
     headers = {'Content-Type': 'application/json'}
     method = 'POST'
 
-    logger.debug("payload: >> {}".format(payload))
+    logger.debug("payload: >> {}".format(payload_data))
 
     req = Request(
         teams_webhook_url,
-        json.dumps(payload).encode('utf-8'),
+        json.dumps(payload_data).encode('utf-8'),
         headers, method=method
     )
 
