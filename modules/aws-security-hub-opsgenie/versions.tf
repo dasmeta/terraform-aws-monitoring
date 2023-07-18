61b0f6d1a717b7ccaba16925d58f1571460bb2f2
Julia Aghamyan <julia@dasmeta.com>
2023-07-18 14:23:26 +0400

    feat(DMVP-2543): Security hub integration with Teams, Slack, Email, Opsgenie

--- modules/aws-security-hub-opsgenie/versions.tf
+++ modules/aws-security-hub-opsgenie/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = ">= 4.0"
     }
 
     # TODO: seems it was supposed to use this provider to create opsgenie topic but it is not used anymore, so commented out, please check if this can be used or removed
