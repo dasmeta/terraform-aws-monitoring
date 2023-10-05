1f2e51c7e76ff688c690e4712bfc2beede10a0a2
Karapetyan, Aram <aram@dasmeta.com>
2023-10-05 18:56:25 +0400

    fix: adjust memory widget

--- modules/dashboard/modules/widgets/container/memory/base.tf
+++ modules/dashboard/modules/widgets/container/memory/base.tf
@@ -6,21 +6,20 @@ module "base" {
 
   coordinates = var.coordinates
 
-  name = "Memory / ${var.container}"
+  name = "Memory"
 
   defaults = {
-    MetricNamespace   = "ContainerInsights"
-    ClusterName       = var.cluster
-    Namespace         = var.namespace
-    PodName           = var.container
-    accountId         = var.account_id
-    anomaly_detection = var.anomaly_detection
+    MetricNamespace = "ContainerInsights"
+    ClusterName     = var.cluster
+    Namespace       = var.namespace
+    PodName         = var.container
+    accountId       = var.account_id
   }
 
   period = var.period
 
   metrics = [
-    { MetricName = "pod_memory_working_set", label = "Current" },
-    { MetricName = "pod_memory_limit", color = "#d62728", label = "Limit" }
+    { MetricName = "pod_memory_limit", color = "#FF0F3C", label = "Limit" },
+    { MetricName = "pod_memory_working_set", color = "#007CEF", label = "Current", anomaly_detection = true }
   ]
 }
