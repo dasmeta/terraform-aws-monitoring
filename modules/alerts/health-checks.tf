7193584ea86c4802f7e1546982bdebe3ed1f4bc8
viktoryathegreat <ghazaryan.victorya@gmail.com>
2024-04-30 10:45:35 +0400

    fix(DMVP-4008): Changed health check alarm sensitivity

--- modules/alerts/health-checks.tf
+++ modules/alerts/health-checks.tf
@@ -4,10 +4,10 @@ locals {
     port              = try(health_check.port, 443)
     type              = try(health_check.type, "HTTPS")
     path              = try(health_check.path, "/")
-    failure_threshold = try(health_check.failure_threshold, "5")
+    failure_threshold = try(health_check.failure_threshold, "1")
     request_interval  = try(health_check.request_interval, "30")
     measure_latency   = try(health_check.measure_latency, false)
-    regions           = try(health_check.regions, ["us-west-1", "us-west-2", "us-east-1", "eu-west-1", "sa-east-1", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1"])
+    regions           = try(health_check.regions, ["us-west-1", "us-west-2", "us-east-1", "eu-west-1"])
     region            = try(health_check.region, data.aws_region.project.name)
     tags              = try(health_check.tags, {})
   } if try(health_check.host, null) != null]
