2cab1514ff842f0c6a5a3fa246b0ee4afa6df38c
Tigran Muradyan <tigran@dasmeta.com>
2026-02-24 16:11:48 +0400

    fix(DMVP-4960): aws provider version support for 6.x version in all module submodules

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.3"
   required_providers {
     aws = {
-      version               = "~> 5.0"
+      version               = ">= 5.0, < 7.0"
       configuration_aliases = [aws, aws.virginia]
     }
   }
